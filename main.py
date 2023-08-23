37ecaf958de9a6b74980305a63aa4b7dacb56f63
GreenBeret <ahmed.rafed99@gmail.com>
2023-08-23 14:13:57 +0200

    refactored code

--- main.py
+++ main.py
@@ -5,55 +5,72 @@ import pygetwindow as gw
 import pytesseract
 from PIL import Image, ImageEnhance
 
-error_codes = ["277", "268", "264", "529", "279", "266", "267", "279"] #edit this list to match the errors you want
+
+WINDOW_NAME = "image" # this is what your window title should start with
+ERROR_CODES = ["277", "268", "264", "529", "279", "266", "267", "279"]  # edit this list to match the errors you want
 output_file = "error_log.txt"
 
 # Define the enhancement factor for brightness and contrast
 BRIGHTNESS_FACTOR = 1.5
 CONTRAST_FACTOR = 1.5
+
+#after running the script, this folder should contain screenshots of your windows
+SCREENSHOT_PATH = "screenshots/"
+
+
 def main():
-
     while True:
-        windows = gw.getWindowsWithTitle('image')
-
-        for window in windows:
-            window_title = window.title
-            window_index = windows.index(window)
-
-            window.minimize()
-            window.restore()
-            time.sleep(0.5)
-
-            if window.isActive:
-                screenshot = pyautogui.screenshot(region=(window.left, window.top, window.width, window.height))
-                enhanced_screenshot = enhance_image(screenshot)
-                enhanced_screenshot.save(f'screenshots/screenshot {window_index}.png')
-
-                ocr_result = perform_ocr(f'screenshots/screenshot {window_index}.png')
-                for error_code in error_codes:
-                    if f"Error Code: {error_code}".replace(" ", "").lower() in ocr_result.replace(" ", "").lower():
-                        error_message = f"Found Error Code: {error_code} in window: {window_title}"
-                        save_error_to_file(error_message)
-                        print(error_message)
-                        window.close()
-                        window_closed_alert = f"closed window{window_title}"
-                        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}:  closed window {window_title}")
-                        save_error_to_file(window_closed_alert)
-
+        for window in gw.getWindowsWithTitle(WINDOW_NAME):
+            process_window(window)
         time.sleep(300)
 
 
+def process_window(window):
+    window_title = window.title
+    window.minimize()
+    window.restore()
+    time.sleep(0.5)
 
+    if window.isActive:
+        screenshot = take_screenshot(window)
+        enhanced_screenshot = enhance_image(screenshot)
+        screenshot_path = save_screenshot(enhanced_screenshot, window)
+
+        ocr_result = perform_ocr(screenshot_path)
+        if check_error_codes(ocr_result, window):
+            close_window(window)
+
+
+def take_screenshot(window):
+    return pyautogui.screenshot(region=(window.left, window.top, window.width, window.height))
+
+
+def save_screenshot(screenshot, window):
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    screenshot_path = f"{SCREENSHOT_PATH}screenshot_{window.title}_{timestamp}.png"
+    screenshot.save(screenshot_path)
+    return screenshot_path
+
+def check_error_codes(ocr_result, window):
+    for error_code in ERROR_CODES:
+        if f"error code: {error_code}".replace(" ", "").lower() in ocr_result.replace(" ", "").lower():
+            error_message = f"Found Error Code: {error_code} in window: {window.title}"
+            save_error_to_file(error_message)
+            print(error_message)
+            return True
+    return False
 
 def perform_ocr(image_path):
     img = Image.open(image_path)
     ocr_result = pytesseract.image_to_string(img)
     return ocr_result
 
+
 def save_error_to_file(error_message):
     timestamp = datetime.datetime.now()
-    with open(output_file,  "a", encoding="utf-8") as file:
+    with open(output_file, "a", encoding="utf-8") as file:
         file.write(timestamp.replace(microsecond=0).strftime('%Y-%m-%d %H:%M:%S') + ":  " + error_message + "\n")
+
 
 def enhance_image(image):
     enhancer = ImageEnhance.Brightness(image)
@@ -63,6 +80,12 @@ def enhance_image(image):
     enhanced_image = enhancer.enhance(CONTRAST_FACTOR)
 
     return enhanced_image
+
+def close_window(window):
+    window.close()
+    window_closed_alert = f"Closed window: {window.title}"
+    save_error_to_file(window_closed_alert)
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {window_closed_alert}")
 
 if __name__ == "__main__":
     main()
